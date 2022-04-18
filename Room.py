eede76ef1fcabff8f77090776cdb00395deec685
joyho796 <joyho796@gmail.com>
2022-04-18 16:01:55 -0400

    dungeon setup

--- Room.py
+++ Room.py
@@ -1,6 +1,9 @@
 class Room:
-    def __init__(roomName):
+    def __init__(self, roomName, north, east, south, west, items, enemies):
         self.roomName = roomName
+        self.adjacentRooms = [north, east, south, west]
         self.items = []
-        self.adjacentRooms = [None, None, None, None]
         self.enemies = []
+
+    def getAdjacent(self):
+        return self.adjacentRooms
